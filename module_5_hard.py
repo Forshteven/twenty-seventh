32c6e2619c43652532136f1c2bc205903b765b70
Forshteven <geocinds@mail.ru>
2024-09-29 18:41:18 +0900

    Update module_5_hard.py

--- module_5_hard.py
+++ module_5_hard.py
@@ -1,9 +1,23 @@
-class UrTube:
-    users = {}
-    videos = {}
-    def __init__(self, current_user):
-        self.current_user = current_user
+class Video:
+    def __init__(self, title, duration, adult_mode=True):
+        self.title = title
+        self.duration = duration
+        self.time_now = True
+        self.adult_mode = adult_mode
 
+
+class User:
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = password
+        self.age = age
+
+
+class UrTube(Video, User):
+    users = []
+    videos = []
+    current_user = True
+        
     def log_in(self, nickname, password):
         while True:
             if nickname in self.users[nickname]:
@@ -17,31 +31,37 @@ class UrTube:
     def register(self, nickname, password, age):
         while True:
             if nickname not in self.users[nickname]:
-                self.users.setdefault(nickname, password, age)
+                new_user = User(nickname, password, age)
+                self.users.append(new_user)
                 print(f"Пользователь {nickname} добавлен")
                 break
             else:
                 print(f"Пользователь {nickname} уже существует")
 
-    def add(self, *args):
-        for i in args
-            if isinstance(i, Video):
-                self.videos.setdefault(i)
+    def add_videos(self, *args):
+        while True:
+            if self.title not in self.videos:
+                new_video = Video(self.title, self.duration, adult_mode=True)
+                self.videos.append(new_video)
+                print(f"Видео {self.title} добавлено")
+                break
+            else:
+                print(f"Видео уже существует")
 
     def get_videos(self, title):
-        return [i for i in self.videos if title in i]
+        if title in self.videos:
+            return self.videos[title]
+        else:
+            print(f"Видео не найдено")
 
 
-class Video:
-    def __init__(self, title, duration, time_now, adult_mode):
-        self.title = title
-        self.duration = duration
-        self.time_now = time_now
-        self.adult_mode = adult_mode
+ur = UrTube(Video, User)
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
+# Добавление видео
+ur.add_videos(v1, v2)
 
-class User:
-    def __init__(self, nickname, password, age):
-        self.nickname = nickname
-        self.password = password
-        self.age = age
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
