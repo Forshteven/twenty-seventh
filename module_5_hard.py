6c37c4229524e6d988535f9d5710485063d6b72f
Forshteven <geocinds@mail.ru>
2024-10-17 11:49:07 +0300

    Add files via upload

--- module_5_hard.py
+++ module_5_hard.py
@@ -5,13 +5,11 @@ class Video:
         self.time_now = 0
         self.adult_mode = adult_mode
 
-
 class User:
     def __init__(self, nickname, password, age):
         self.nickname = nickname
         self.password = password
         self.age = age
-
 
 class UrTube:
     def __init__(self):
@@ -50,7 +48,7 @@ class UrTube:
 
     def add_videos(self, *videos):
         for video in videos:
-            if not any(v.title == video.title.lower() for v in self.videos):
+            if not any(v.title == video.title for v in self.videos):
                 self.videos.append(video)
                 print(f"Видео {video.title} добавлено")
             else:
@@ -68,19 +66,22 @@ class UrTube:
         if not self.current_user:
             print("Войдите в аккаунт, чтобы смотреть видео")
             return
+        title = title.lower()
+        video_found = False
         for video in self.videos:
-            if video.adult_mode and self.current_user.age < 18:
-                print("Вам нет 18 лет, пожалуйста покиньте страницу")
-                return
-            elif title.lower() in video.title:
+            if video.title == title:
+                video_found = True
+                if self.current_user.age < 18 and video.adult_mode:
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                    return
                 while video.time_now < video.duration:
                     print(f"{video.time_now + 1}", end=' ')
                     video.time_now += 1  # Симуляция просмотра видео
                 print("\nКонец видео")
                 video.time_now = 0  # Сбрасываем текущее время просмотра
                 return
-            else:
-                print("Видео не найдено для просмотра")
+        if not video_found:
+            print("Видео не найдено для просмотра")
 
 
 #
